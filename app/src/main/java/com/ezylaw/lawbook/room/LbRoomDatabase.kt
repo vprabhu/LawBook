51d56d473d2f19a9bab59cc7ac01f81ac30c5caf
abdulhamid <abdul.hamid@paycraftsol.com>
2022-05-01 02:49:11 +0530

    rename

--- app/src/main/java/com/ezylaw/lawbook/room/LbRoomDatabase.kt
+++ app/src/main/java/com/ezylaw/lawbook/room/LbRoomDatabase.kt
@@ -4,12 +4,19 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
-import com.ezylaw.lawbook.model.Particular
-import com.ezylaw.lawbook.model.User
+import com.ezylaw.lawbook.model.*
 import kotlinx.coroutines.CoroutineScope
 
 @Database(
-    entities = [User::class, Particular::class],
+    entities = [
+        Address::class,
+        Cases::class,
+        Comments::class,
+        Particular::class,
+        Payment::class,
+        Slots::class,
+        User::class
+    ],
     version = 1,
     exportSchema = false
 ) //exportSchema = false true In case of Migration
